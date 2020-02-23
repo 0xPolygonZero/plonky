6804d6073b88d9f4e480b06ff2609c6dd4d92d22
Daniel Lubarov <daniel@lubarov.com>
2020-02-23 00:20:07 -0800

    More G1 benchmarks

--- benches/bls12.rs
+++ benches/bls12.rs
@@ -3,27 +3,34 @@ use criterion::criterion_group;
 use criterion::criterion_main;
 use num::BigUint;
 
-use plonky::{Bls12Base, Bls12Scalar, G1ProjectivePoint, mul_6_6};
+use plonky::{Bls12Base, Bls12Scalar, G1ProjectivePoint, mul_6_6, G1AffinePoint, G1_GENERATOR_AFFINE};
 
 fn criterion_benchmark(c: &mut Criterion) {
     // We want a scalar with a Hamming weight of 0.5, to simulate the "average case".
     let s_part = 0b1010101010101010101010101010101010101010101010101010101010101010u64;
     let s = Bls12Scalar { limbs: [s_part, s_part, s_part, s_part] };
 
-    let g_x = Bls12Base { limbs: [11111111, 22222222, 33333333, 44444444, 55555555, 66666666] };
-    let g_y = Bls12Base { limbs: [44444444, 55555555, 66666666, 77777777, 88888888, 99999999] };
-    let g = G1ProjectivePoint { x: g_x, y: g_y, z: Bls12Base::ONE };
+    let g_affine = G1_GENERATOR_AFFINE;
+    let g_projective = G1ProjectivePoint { x: g_affine.x * 2, y: g_affine.y * 2, z: Bls12Base::TWO };
 
-    c.bench_function("BLS12 G1 addition", move |b| b.iter(|| {
-        black_box(g) + black_box(g);
+    c.bench_function("BLS12 G1 affine + affine = projective addition", move |b| b.iter(|| {
+        let _result: G1ProjectivePoint = black_box(g_affine) + black_box(g_affine);
     }));
 
-    c.bench_function("BLS12 G1 doubling", move |b| b.iter(|| {
-        black_box(g).double();
+    c.bench_function("BLS12 G1 projective + affine = projective addition", move |b| b.iter(|| {
+        let _result: G1ProjectivePoint = black_box(g_projective) + black_box(g_affine);
     }));
 
-    c.bench_function("BLS12 G1 multiplication", move |b| b.iter(|| {
-        black_box(s) * black_box(g);
+    c.bench_function("BLS12 G1 projective + projective = projective addition", move |b| b.iter(|| {
+        let _result: G1ProjectivePoint = black_box(g_projective) + black_box(g_projective);
+    }));
+
+    c.bench_function("BLS12 G1 projective doubling", move |b| b.iter(|| {
+        black_box(g_projective).double();
+    }));
+
+    c.bench_function("BLS12 G1 projective multiplication", move |b| b.iter(|| {
+        black_box(s) * black_box(g_projective);
     }));
 }
 
