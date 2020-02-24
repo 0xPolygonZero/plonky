9aca577e4d4a03f52f202c27e6130caf5d50a433
Daniel Lubarov <daniel@lubarov.com>
2020-02-24 13:17:38 -0800

    Remove BigUint benches

--- benches/field_arithmetic.rs
+++ benches/field_arithmetic.rs
@@ -1,7 +1,6 @@
 use criterion::{black_box, Criterion};
 use criterion::criterion_group;
 use criterion::criterion_main;
-use num::BigUint;
 
 use plonky::{Bls12Base, mul_6_6};
 
@@ -12,35 +11,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     let x_bls12base = Bls12Base::from_canonical(x);
     let y_bls12base = Bls12Base::from_canonical(y);
 
-    let x_biguint = u64_slice_to_biguint(&x);
-    let y_biguint = u64_slice_to_biguint(&y);
-    let m_biguint = u64_slice_to_biguint(&Bls12Base::ORDER);
-
-    {
-        let x_biguint = x_biguint.clone();
-        let y_biguint = y_biguint.clone();
-        c.bench_function("BigUint widening multiplication", move |b| b.iter(|| {
-            let x_biguint = x_biguint.clone();
-            let y_biguint = y_biguint.clone();
-            black_box(x_biguint) * black_box(y_biguint);
-        }));
-    }
-
     c.bench_function("[u64] widening multiplication", move |b| b.iter(|| {
         mul_6_6(black_box(x), black_box(y));
     }));
-
-    {
-        let x_biguint = x_biguint.clone();
-        let y_biguint = y_biguint.clone();
-        let m_biguint = m_biguint.clone();
-        c.bench_function("BigUint field multiplication", move |b| b.iter(|| {
-            let x_biguint = x_biguint.clone();
-            let y_biguint = y_biguint.clone();
-            let m_biguint = m_biguint.clone();
-            black_box(x_biguint) * black_box(y_biguint) % black_box(m_biguint);
-        }));
-    }
 
     c.bench_function("Bls12Base field addition", move |b| b.iter(|| {
         black_box(y_bls12base) + black_box(x_bls12base);
@@ -61,16 +34,6 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Bls12Base field inversion", move |b| b.iter(|| {
         black_box(x_bls12base).multiplicative_inverse()
     }));
-}
-
-fn u64_slice_to_biguint(n: &[u64]) -> BigUint {
-    let mut bytes_le = Vec::new();
-    for n_i in n {
-        for j in 0..8 {
-            bytes_le.push((n_i >> j * 8) as u8);
-        }
-    }
-    BigUint::from_bytes_le(&bytes_le)
 }
 
 criterion_group!(benches, criterion_benchmark);
