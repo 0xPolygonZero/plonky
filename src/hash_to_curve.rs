1e01b3773669ac4380492ff48d85fb5d30c79137
Daniel Lubarov <daniel@lubarov.com>
2020-05-09 01:21:22 -0700

    Revise hash-to-point to avoid the collision vuln that William pointed out

--- src/hash_to_curve.rs
+++ src/hash_to_curve.rs
@@ -1,16 +1,22 @@
-use crate::{AffinePoint, Curve, Field, PRFBasedPRG, RescuePrf, PRG};
+use crate::{AffinePoint, Curve, Field, rescue_sponge};
 
-pub fn hash_u32_to_curve<C: Curve>(seed: u32) -> AffinePoint<C> {
-    hash_base_field_to_curve(C::BaseField::from_canonical_u32(seed))
+pub fn hash_u32_to_curve<C: Curve>(seed: u32, security_bits: usize) -> AffinePoint<C> {
+    let seed_f = C::BaseField::from_canonical_u32(seed);
+    hash_base_field_to_curve(seed_f, security_bits)
 }
 
-pub fn hash_base_field_to_curve<C: Curve>(mut seed: C::BaseField) -> AffinePoint<C> {
-    // This is the try-and-increment method from https://eprint.iacr.org/2009/226.pdf
-    let prf = RescuePrf::default();
-    let mut prg = PRFBasedPRG::seeded(prf, seed);
+pub fn hash_base_field_to_curve<C: Curve>(
+    mut seed: C::BaseField,
+    security_bits: usize,
+) -> AffinePoint<C> {
+    // Based on the MapToGroup method of BLS.
+    let mut i = 0;
     loop {
-        let x = prg.next_field();
-        let y_neg = prg.next_bool();
+        // Let (x, y_neg) = H(seed, i).
+        let inputs = vec![seed, C::BaseField::from_canonical_u32(i)];
+        let outputs = rescue_sponge(inputs, 2, security_bits);
+        let x = outputs[0];
+        let y_neg = outputs[1].to_canonical_bool_vec()[0];
 
         // We compute x^3 + a x + b, then check if it's a square in the field. If it is (which
         // occurs with a probability of ~0.5), we have found a point on the curve.
@@ -21,6 +27,8 @@ pub fn hash_base_field_to_curve<C: Curve>(mut seed: C::BaseField) -> AffinePoint
             }
             return AffinePoint::nonzero(x, y)
         }
+
+        i += 1;
     }
 }
 
@@ -32,7 +40,7 @@ mod tests {
     fn test_hash_u32_to_point() {
         // Just make sure it runs with no errors.
         for i in 0..5 {
-            hash_u32_to_curve::<Tweedledum>(i);
+            hash_u32_to_curve::<Tweedledum>(i, 128);
         }
     }
 }
