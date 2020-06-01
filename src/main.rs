f9b75c2d03cf512a4b6f280201df960ea44c2fa7
Daniel Lubarov <daniel@lubarov.com>
2020-06-01 01:31:04 -0700

    Fixes related to gate_constants
    gate_constants is index by gate index and then wire index, but the code that IFFTs it expected the opposite.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::time::Instant;
 
-use plonky::{recursive_verification_circuit, Tweedledum, Tweedledee};
+use plonky::{recursive_verification_circuit, Tweedledum, Tweedledee, PartialWitness, Curve};
 
 fn main() {
     let degree_pow = 13;
@@ -12,15 +12,21 @@ fn main() {
     println!("Gate count: {}", recursive_circuit.circuit.degree());
     println!();
 
+    // Dummy inputs.
+    let mut inputs = PartialWitness::new();
+    inputs.set_point_target(recursive_circuit.proof.c_plonk_z, Tweedledum::GENERATOR_AFFINE);
+    inputs.set_point_target(recursive_circuit.proof.halo_g, Tweedledum::GENERATOR_AFFINE);
+    // TODO: Populate other targets with dummy data.
+
     println!("Generating witness...");
     let start = Instant::now();
-    // TODO
+    let witness = recursive_circuit.circuit.generate_witness(inputs);
     println!("Finished in {}s", start.elapsed().as_secs_f64());
     println!();
 
     println!("Generating proof...");
     let start = Instant::now();
-    // TODO
+    recursive_circuit.circuit.generate_proof::<Tweedledum>(witness);
     println!("Finished in {}s", start.elapsed().as_secs_f64());
     println!();
 
