632205329c1b9b2e8fcf3e39e2d02565d1b0c425
Daniel Lubarov <daniel@lubarov.com>
2020-06-27 12:14:09 -0700

    Tweak MSM timing

--- src/bin/msms.rs
+++ src/bin/msms.rs
@@ -11,8 +11,8 @@ fn main() {
     let min_window_size = 11;
     let max_window_size = 12;
     let min_threads = 1;
-    let max_threads = 100;
-    let repetitions = 3;
+    let max_threads = 50;
+    let repetitions = 5;
     let terms_log = 14;
     let terms = 1 << terms_log;
 
@@ -26,19 +26,19 @@ fn main() {
         for threads in min_threads..=max_threads {
             let mut durations = Vec::new();
             for _ in 0..repetitions {
-                durations.push(time_msm(terms, window_size, threads, &precomputation));
+                durations.push(time_msm(terms, threads, &precomputation));
             }
             durations.sort();
             let average_duration = durations.iter().sum::<Duration>() / durations.len() as u32;
-            let median_duration = durations[durations.len() / 2];
-            println!("MSMs with terms=2^{}, threads={}, window_size={}: average={:.4}s, median={:.4}s",
+            let average_secs = average_duration.as_secs_f64();
+            println!("MSMs with terms=2^{}, threads={}, window_size={}: avg={:.4}s, avg*threads={:.4}s",
                      terms_log, threads, window_size,
-                     average_duration.as_secs_f64(), median_duration.as_secs_f64());
+                     average_secs, average_secs * threads as f64);
         }
     }
 }
 
-fn time_msm(terms: usize, window_size: usize, threads: usize, precomputation: &MsmPrecomputation<C>) -> Duration {
+fn time_msm(terms: usize, threads: usize, precomputation: &MsmPrecomputation<C>) -> Duration {
     let scalars: Vec<_> = (0..terms)
         .map(|_| SF::rand())
         .collect();
